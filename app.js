ba04462c96bd50a405fb7afdd9bf65af94b3a30d
Jamie Kinkead <jkinkead@gmail.com>
2018-11-02 11:50:57 -0700

    port fixed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(morgan('dev'))
 app.use(bodyParser.json())
