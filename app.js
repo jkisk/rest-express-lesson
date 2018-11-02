f8e8217d01825cf2d4af5eb01dcc132bbc6b6afd
Jamie Kinkead <jkinkead@gmail.com>
2018-11-02 11:41:19 -0700

    in progress

--- app.js
+++ app.js
@@ -23,11 +23,13 @@ const authors = [
   }
 ]
 
+
+
 /* Add RESTful routes for the following:
 
   - GET author by index
     - should respond 404 if author at index does not exist
-  - POST author  
+   - POST author  
   - GET all books by author
   - POST book by author
     - should respond 404 if author does not exist
@@ -39,11 +41,43 @@ const authors = [
   Bonus challenge: Make middleware that checks if the requested author index exists, respond 404 if not.
 */
 
+app.get('/authors', (req,res) => {
+  res.send(authors)
+})
 
+app.get('/authors/:id', (req,res,next) => {
+  
+const authorId = req.params.id
+  if(!authors[authorId]) {
+  next()
+  } 
+  
+res.send(authors[authorId])
+ 
+})
+
+app.post('/authors', (req, res, next) => {
+  const newAuthor = req.body
+  authors.push(newAuthor)
+  res.status(201).send(authors)
+})
+
+app.patch('/authors/:id', (req,res,next) => {
+  
+  const authorId = req.params.id
+  const newData = req.body
+  
+  if(!authors[authorId]) {
+  next()
+  } 
+  authors.push(newData)
+  res.status(201).send(authors)
+})
 
 app.use((req, res) => {
   res.sendStatus(404)
 })
+
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
